c1e8028c037ab64621db5717c3903b1d81003117
Patrick Gough <jUnion444@gmail.com>
2020-09-20 17:25:47 -0400

    Made models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+#Model to represent a state.
+class State(models.Model):
+    name = models.CharField(max_length=100)
+    data = models.FloatField()
+
+#Blogpost Tag (or for anything else)
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    state = models.ForeignKey(State, null=True)
+    #If an article is tagged with a state, it should be visible in that state's
+    #States and their tags should be created in bulk
+    
+#Relatively simple if there are no authors
+class Blogpost(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField() #When it appears on the homepage/state profile
+    content = models.TextField() #Actual markdown content
+    tags = models.ManyToManyField(Tag)
